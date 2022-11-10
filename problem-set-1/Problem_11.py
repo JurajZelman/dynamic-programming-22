2412cf321ddcd43fee5cf0028a8393b69f20113a
Juraj Zelman <zelman.juraj@gmail.com>
2022-11-10 15:19:50 +0100

    Add results processing and the transition plot

--- problem-set-1/Problem_11.py
+++ problem-set-1/Problem_11.py
@@ -1,6 +1,9 @@
 """Copyright (c) 2022 Juraj Zelman"""
 
+import matplotlib.patches as mpatches
+import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
 p_w = 1 / 3  # Probability of the disturbance w being 1
 n_timesteps = 10  # Number of timesteps
@@ -33,3 +36,45 @@ for k in range(n_timesteps - 1, -1, -1):
         # Find optimal cost-to-go and control
         J_opt[state, k] = np.min(cost_to_go)
         u_opt[state, k] = Uk[np.argmin(cost_to_go)]
+
+# Save results to pandas DataFrame
+row_labels = [f"x_{i}" for i in range(n_states)]
+column_labels = [f"k = {i}" for i in range(n_timesteps)]
+df_controls = pd.DataFrame(u_opt, index=row_labels, columns=column_labels)
+column_labels.append(f"k = {n_timesteps}")
+df_cost = pd.DataFrame(J_opt, index=row_labels, columns=column_labels)
+
+# Print the results
+print("Optimal final cost")
+print(df_cost)
+print()
+print("Optimal controls")
+print(df_controls)
+
+# Plot the optimal policy (for w_k = 1)
+p, q = np.meshgrid(np.arange(n_timesteps + 1), x)
+plt.figure(figsize=(7, 4))
+plt.scatter(p, q, marker="o")
+
+# Plot the optimal transitions
+x_plus_u = x + np.transpose(u_opt[:, 1])
+for i in range(n_timesteps):
+    x_plus_u = x + np.transpose(u_opt[:, i])
+    plt.plot([i, i + 1], [x, x_plus_u], "k")
+
+# Plot x_ref(k)
+k = np.arange(n_timesteps + 1)
+plt.plot(k, np.power(k - 5, 2), "r")
+
+# Format the plot
+plt.xlabel("Time step k")
+plt.ylabel("State x_k")
+plt.xlim((-1, n_timesteps + 1))
+plt.ylim((x[0] - 1, x[-1] + 1))
+
+p1 = mpatches.Patch(color="red", label="x_ref(k)")
+p2 = mpatches.Patch(color="black", label="x_k+1 = x_k + u_k")
+plt.legend(handles=[p1, p2], loc="upper left", bbox_to_anchor=(1.04, 1))
+plt.tight_layout()
+
+plt.show()
